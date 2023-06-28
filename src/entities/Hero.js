32bbe0a55a608b583a58857c78cad0fec028586e
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-06-28 19:52:07 +0200

    double jump

--- src/entities/Hero.js
+++ src/entities/Hero.js
@@ -12,6 +12,46 @@ class Hero extends Phaser.GameObjects.Sprite {
     this.body.setCollideWorldBounds(true);
     this.body.setSize(12, 40);
     this.body.setOffset(12, 23);
+    this.body.setMaxVelocity(200, 350);
+    this.body.setDragX(750);
+
+    this.keys = scene.cursorKeys;
+  }
+
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+
+    if (this.keys.left.isDown) {
+      this.body.setAccelerationX(-1000);
+      this.body.setOffset(6, 23);
+      this.setFlipX(true);
+    } else if (this.keys.right.isDown) {
+      this.body.setAccelerationX(1000);
+      this.body.setOffset(12, 23);
+      this.setFlipX(false);
+    } else {
+      this.body.setAcceleration(0);
+    }
+
+    const didPressJump = Phaser.Input.Keyboard.JustDown(this.keys.up);
+
+    if (didPressJump) {
+      if (this.body.onFloor()) {
+        this.canDoubleJump = true;
+        this.body.setVelocityY(-450);
+      } else if (this.canDoubleJump) {
+        this.body.setVelocityY(-250);
+        this.canDoubleJump = false;
+      }
+    }
+
+    if (this.body.onFloor) {
+      this.canDoubleJump = false;
+    }
+
+    if (!this.keys.up.isDown && this.body.velocity.y < -250) {
+      this.body.setVelocityY(-150);
+    }
   }
 }
 
